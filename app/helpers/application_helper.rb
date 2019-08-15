ec74f41748357fa40d500a75151796cd313a62aa
Arash Naveed <arsh.nvd@gmail.com>
2019-08-15 17:56:25 +0500

    Display error messages on form submit if any.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def error_messages_for(record)
+    return unless record.errors.any?
+
+    content_tag :ul, class: 'alert alert-danger px-4' do
+      record.errors.full_messages.each do |msg|
+        concat(tag.li msg)
+      end
+    end
+  end
 end
